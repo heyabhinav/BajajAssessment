d094d1cf8611f40cdb1db8831a682922b86416fb
Abhinav Gupta <101443613+heyabhinav@users.noreply.github.com>
2024-03-18 11:21:19 +0530

    InitialCommit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) =>{
+  res.send("Hello World!");
+})
+
+const template = {
+    "is_success" : true,
+    "user_id" : "Abhinav_Gupta_02082003",
+    "email" : "abhinav0050.be21@chitkara.edu.in",
+    "roll_number":"2110990050",
+    "odd_numbers": [],
+    "even_numbers": [],
+    "alphebets": [],
+  }
+
+app.post('/bfhl', (req, res)=>{
+  const {data} = req.body;
+  const response = template;
+  
+  for(var i=0; i<data.length; i++){
+    var ele = parseInt(data[i]);
+    if(isNaN(ele)){
+      response.alphebets.push(data[i].toUpperCase());
+    }else{
+      if(ele%2==0){
+        response.even_numbers.push(ele);
+      }else{
+        response.odd_numbers.push(ele);
+      }
+    }
+    
+  }
+  res.json(response);
+})
+
+app.listen(8000, () => {
+  console.log('server started');
+})
